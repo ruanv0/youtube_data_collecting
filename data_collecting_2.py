fcb66d9350e583d8d90514e4f5121c23ba395fa2
ruanv0 <ruanvictordossantoscorrea128@gmail.com>
2024-07-19 21:30:55 -0300

    2024.07.19.6

--- data_collecting_2.py
+++ data_collecting_2.py
@@ -11,7 +11,7 @@ from utils import add9, interpolate_9
 
 
 def collect_subs_data(item: str) -> str:
-    sdata = ''
+    string_data = ''
     optionsi = Options()
     optionsi.add_argument("--log-level=3")
     optionsi.add_experimental_option("prefs", {'profile.managed_default_content_settings.images': 2})
@@ -21,38 +21,38 @@ def collect_subs_data(item: str) -> str:
     try:
         WebDriverWait(driver, 4).until(expected_conditions.presence_of_element_located(
             (By.XPATH, '//div[@id="profile_show_subscriber_count"]')))
-        elem = driver.find_element(by='xpath', value='//div[@id="profile_show_subscriber_count"]')
-        sdata = elem.text
+        element_0 = driver.find_element(by='xpath', value='//div[@id="profile_show_subscriber_count"]')
+        string_data = element_0.text
     except TimeoutException:
         try:
             WebDriverWait(driver, 4).until(expected_conditions.presence_of_element_located(
                 (By.XPATH, '//span[@class="yt-subscription-button-subscriber-count-branded-horizontal subscribed"]')))
-            elem2 = driver.find_element(by='xpath', value='//span[@class="yt-subscription-button-subscriber-count-branded-horizontal subscribed"]')
-            sdata = elem2.text
+            element_1 = driver.find_element(by='xpath', value='//span[@class="yt-subscription-button-subscriber-count-branded-horizontal subscribed"]')
+            string_data = element_1.text
         except TimeoutException:
             try:
                 WebDriverWait(driver, 4).until(expected_conditions.presence_of_element_located(
                     (By.XPATH, '//span[@class="stat-value"]')))
                 elem3 = driver.find_element(by='xpath', value='//span[@class="stat-value"]')
-                sdata = elem3.text
+                string_data = elem3.text
             except TimeoutException:
                 try:
                     WebDriverWait(driver, 4).until(expected_conditions.presence_of_element_located(
                         (By.XPATH, '//strong[@id="user-profile-subscriber-count"]')))
                     elem4 = driver.find_element(by='xpath', value='//strong[@id="user-profile-subscriber-count"]')
-                    sdata = elem4.text
+                    string_data = elem4.text
                 except TimeoutException:
                     pass
-    return sdata.replace(',', '').replace('.', '').replace(' ', '')
+    return string_data.replace(',', '').replace('.', '').replace(' ', '')
 
 
-def search_youtube_page(user: str, name: str):
+def search_youtube_page(user_id: str, name: str):
     optionsi = Options()
     optionsi.add_argument("--log-level=3")
     optionsi.add_experimental_option("prefs", {'profile.managed_default_content_settings.images': 2})
     service = Service(executable_path='msedgedriver')
     driver = webdriver.Chrome(service=service, options=optionsi)
-    website = f'https://web.archive.org/web/*/http://youtube.com/user/{user}'
+    website = f'https://web.archive.org/web/*/http://youtube.com/user/{user_id}'
     dates = []
     subs = []
     years = []
@@ -102,13 +102,13 @@ def search_youtube_page(user: str, name: str):
     write_csv('test01.csv', e[0], [e[1]], [[name, '']])
     '''
     try:
-        prochannel(name)
+        search_channel(name)
     except:
-        print(f'Não foi pessivel pesquisar o canal {name}')'''
+        print(f'Unable to find the channel {name}')'''
 
 
 if __name__ == '__main__':
-    j = ['AuthenticGames']
+    users_ids = ['AuthenticGames']
     names = ['AuthenticGames']
-    for i in range(0, len(j)):
-        search_youtube_page(j[i], names[i])
+    for i in range(0, len(users_ids)):
+        search_youtube_page(users_ids[i], names[i])
